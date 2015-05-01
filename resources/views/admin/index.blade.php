2a054613da92e18ca1caabe8e89cd7463b28fc84
satriashp <satria.unp@gmail.com>
2015-05-01 09:53:49 +0700

    config java scripts

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,5 @@
     @include('flash::message')
     Welcome
     <div id="chart" style="width:100%; height:400px;"></div>
+    <div id="pie" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
 @endsection
